16e350bd0f4d04de006ac7076cb08768b45370eb
whdevops codebuild <whdevops@whitehawk.com>
2022-09-08 21:29:12 +0000

    Updates

--- _nuxt/static/1662672111/blog/breaking-down-five-2018-breaches-and-what-they-mean-for-security-in-2019/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662672111",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fbreaking-down-five-2018-breaches-and-what-they-mean-for-security-in-2019",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
