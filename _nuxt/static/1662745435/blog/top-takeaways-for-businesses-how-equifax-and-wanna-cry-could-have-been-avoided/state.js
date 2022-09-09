4553085320805ce3243bf8914ada1d45cc9b6fad
whdevops codebuild <whdevops@whitehawk.com>
2022-09-09 17:44:12 +0000

    Updates

---
+++ _nuxt/static/1662745435/blog/top-takeaways-for-businesses-how-equifax-and-wanna-cry-could-have-been-avoided/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662745435",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Ftop-takeaways-for-businesses-how-equifax-and-wanna-cry-could-have-been-avoided",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
---
+++ _nuxt/static/1662745435/blog/white-hawk-top-20-innovative-cmmc-companies-1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662745435",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fwhite-hawk-top-20-innovative-cmmc-companies-1",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
