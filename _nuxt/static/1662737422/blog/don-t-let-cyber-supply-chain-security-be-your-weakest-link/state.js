ce25576f9b124fdfd8158523cbb0ca023f9d7222
whdevops codebuild <whdevops@whitehawk.com>
2022-09-09 15:37:46 +0000

    Updates

--- _nuxt/static/1662737422/blog/don-t-let-cyber-supply-chain-security-be-your-weakest-link/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662737422",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fdon-t-let-cyber-supply-chain-security-be-your-weakest-link",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
---
+++ _nuxt/static/1662737848/blog/black-friday-and-cyber-monday-top-10-cybersecurity-tips/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662737848",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fblack-friday-and-cyber-monday-top-10-cybersecurity-tips",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
