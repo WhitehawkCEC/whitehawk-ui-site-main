7b248794f33baed3d68f696edc6c21ba5486aeb7
whdevops codebuild <whdevops@whitehawk.com>
2022-09-09 22:53:34 +0000

    Updates

--- _nuxt/static/1662758356/blog/study-shows-increase-in-cybersecurity-incidents/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662758356",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fstudy-shows-increase-in-cybersecurity-incidents",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
---
+++ _nuxt/static/1662763996/blog/protect-your-business-from-the-next-ransomware-attack/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662763996",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fprotect-your-business-from-the-next-ransomware-attack",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
