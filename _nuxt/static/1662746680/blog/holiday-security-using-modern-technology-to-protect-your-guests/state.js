a37dc294b14d97410b20aced3f7dbf67571d1c08
whdevops codebuild <whdevops@whitehawk.com>
2022-09-09 21:19:34 +0000

    Updates

--- _nuxt/static/1662746680/blog/holiday-security-using-modern-technology-to-protect-your-guests/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662746680",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fholiday-security-using-modern-technology-to-protect-your-guests",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
--- _nuxt/static/1662746680/logout/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1662746680",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Flogout",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{},"impersonated-user":{user:a}}}}(null));
