0694b3ecf01c1dde5a6cb78e51199266f68aab35
whdevops codebuild <whdevops@whitehawk.com>
2022-09-13 18:57:22 +0000

    Updates

---
+++ _nuxt/static/1663095424/blog/how-a-diverse-workforce-will-improve-the-cybersecurity-industry/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663095424",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fhow-a-diverse-workforce-will-improve-the-cybersecurity-industry",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
