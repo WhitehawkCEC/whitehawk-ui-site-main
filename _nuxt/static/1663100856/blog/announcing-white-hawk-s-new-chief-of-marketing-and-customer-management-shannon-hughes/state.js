e3f247bfb1519d3cf98608055f5964a3aaeb8c38
whdevops codebuild <whdevops@whitehawk.com>
2022-09-13 20:27:55 +0000

    Updates

---
+++ _nuxt/static/1663100856/blog/announcing-white-hawk-s-new-chief-of-marketing-and-customer-management-shannon-hughes/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663100856",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fannouncing-white-hawk-s-new-chief-of-marketing-and-customer-management-shannon-hughes",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
