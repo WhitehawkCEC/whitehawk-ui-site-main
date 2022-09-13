667409f7d3b449d9053e00332032deb95077d338
whdevops codebuild <whdevops@whitehawk.com>
2022-09-13 20:13:51 +0000

    Updates

--- _nuxt/static/1663099499/blog/blockchain-s-killer-app-making-trade-wars-obsolete/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663099499",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fblockchain-s-killer-app-making-trade-wars-obsolete",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
