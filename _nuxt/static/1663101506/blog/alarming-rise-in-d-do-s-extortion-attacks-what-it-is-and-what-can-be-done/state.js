98231836419efe435bbe75a49f7ca4bde52fb24b
whdevops codebuild <whdevops@whitehawk.com>
2022-09-13 20:38:44 +0000

    Updates

---
+++ _nuxt/static/1663101506/blog/alarming-rise-in-d-do-s-extortion-attacks-what-it-is-and-what-can-be-done/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663101506",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Falarming-rise-in-d-do-s-extortion-attacks-what-it-is-and-what-can-be-done",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
