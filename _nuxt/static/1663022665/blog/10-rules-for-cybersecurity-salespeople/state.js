466378ac11379d401de98e7528c49074e4c304c2
whdevops codebuild <whdevops@whitehawk.com>
2022-09-13 00:21:06 +0000

    Updates

--- _nuxt/static/1663022665/blog/10-rules-for-cybersecurity-salespeople/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663022665",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002F10-rules-for-cybersecurity-salespeople",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
---
+++ _nuxt/static/1663028449/blog/family-office-cyber-risk-2022-dyanmics-and-best-practices/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663028449",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Ffamily-office-cyber-risk-2022-dyanmics-and-best-practices",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
