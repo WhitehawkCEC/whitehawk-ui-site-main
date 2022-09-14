949610e2492594ff971a5917f99d652e2fb51c70
whdevops codebuild <whdevops@whitehawk.com>
2022-09-14 21:47:33 +0000

    Updates

--- _nuxt/static/1663184241/blog/apt-41-targeting-u-s-state-and-local-governments/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663184241",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fapt-41-targeting-u-s-state-and-local-governments",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
---
+++ _nuxt/static/1663192036/blog/rane-podcast-making-cyber-risk-rating-and-reporting-a-systemic-part-of-doing-business/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663192036",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Frane-podcast-making-cyber-risk-rating-and-reporting-a-systemic-part-of-doing-business",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
