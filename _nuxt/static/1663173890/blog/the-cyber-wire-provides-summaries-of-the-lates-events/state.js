2e6d29ca75863202cacc7ff0c10e634cfb09c64c
whdevops codebuild <whdevops@whitehawk.com>
2022-09-14 18:16:07 +0000

    Updates

--- _nuxt/static/1663173890/blog/the-cyber-wire-provides-summaries-of-the-lates-events/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663173890",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fthe-cyber-wire-provides-summaries-of-the-lates-events",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
---
+++ _nuxt/static/1663179348/blog/australian-parliament-recommends-new-best-practices-to-improve-cyber-security/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663179348",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Faustralian-parliament-recommends-new-best-practices-to-improve-cyber-security",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
