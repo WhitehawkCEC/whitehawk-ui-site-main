522c9b73dad59340acaa73536e72204ccbbab5fa
whdevops codebuild <whdevops@whitehawk.com>
2022-09-15 06:14:22 +0000

    Updates

--- _nuxt/static/1663221079/admin/users/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663221079",layout:"default",error:a,serverRendered:true,routePath:"\u002Fadmin\u002Fusers",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{},"active-company":{},"active-user":{},"impersonated-user":{user:a}}}}(null));
