ba505c8d469c88a328ee30586c351f45071d3943
whdevops codebuild <whdevops@whitehawk.com>
2022-09-15 14:21:27 +0000

    Updates

---
+++ _nuxt/static/1663251673/training-and-education/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663251673",layout:"landing",error:a,serverRendered:true,routePath:"\u002Ftraining-and-education",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
