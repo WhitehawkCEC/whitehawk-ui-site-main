45c120dabe92f55f99feeb29f4e2f6f17ac41722
whdevops codebuild <whdevops@whitehawk.com>
2022-09-15 00:47:50 +0000

    Updates

--- _nuxt/static/1663197944/blog/securing-your-identity-this-holiday-season/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663197944",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fsecuring-your-identity-this-holiday-season",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
--- _nuxt/static/1663197944/blog/white-hawk-top-20-innovative-cmmc-companies-1/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663197944",layout:"siteMain",error:a,serverRendered:true,routePath:"\u002Fblog\u002Fwhite-hawk-top-20-innovative-cmmc-companies-1",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
