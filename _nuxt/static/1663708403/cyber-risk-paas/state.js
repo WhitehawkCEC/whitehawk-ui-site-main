d7058bf7cb138691aee09ec4aa9384713290d196
whdevops codebuild <whdevops@whitehawk.com>
2022-09-20 21:19:47 +0000

    Updates

--- _nuxt/static/1663708403/cyber-risk-paas/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663708403",layout:"landing",error:a,serverRendered:true,routePath:"\u002Fcyber-risk-paas",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{},pinia:{user:{}}}}(null));
