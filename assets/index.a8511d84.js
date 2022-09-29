9af7a99a545af8a273a882e8d694805a6e347df6
whdevops codebuild <whdevops@whitehawk.com>
2022-09-29 14:31:29 +0000

    Updates

--- assets/index.a8511d84.js
+++
@@ -1,1 +0,0 @@
-import{t as r}from"./index.fa4b9d28.js";import{ap as m}from"./index.b49ad680.js";function n(t,a){m(2,arguments);var i=r(t),f=r(a),e=i.getTime()-f.getTime();return e>0?-1:e<0?1:e}export{n as c};
--- assets/useAllCompanyAccess.6a08f0a3.js
+++
@@ -1,1 +0,0 @@
-import{ai as s,aP as e,aQ as c}from"./index.b49ad680.js";const n={list(){return s(["company-access"],()=>e.get(`${c}/company-access`).then(a=>a.data))}};export{n as u};
