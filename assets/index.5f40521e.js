7c4721479e7d49a8acd1faf6d57cf4575f510ff0
whdevops codebuild <whdevops@whitehawk.com>
2022-10-14 06:25:46 +0000

    Updates

---
+++ assets/index.5f40521e.js
@@ -0,0 +1,1 @@
+import{an as o,bG as e}from"./index.382abe41.js";import{a as n}from"./index.d88b49da.js";function d(r,a){o(2,arguments);var t=e(a);return n(r,t*12)}export{d as a};
--- assets/useAllCompanyAccess.8b959fa3.js
+++
@@ -1,1 +0,0 @@
-import{ag as s,aP as e,aQ as c}from"./index.0f72a133.js";const n={list(){return s(["company-access"],()=>e.get(`${c}/company-access`).then(a=>a.data))}};export{n as u};
