1768571c2010385e553ce3ec422c9f9425deaa6e
whdevops codebuild <whdevops@whitehawk.com>
2022-10-14 16:53:43 +0000

    Updates

--- assets/index.47ca3192.js
+++
@@ -1,1 +0,0 @@
-import{an as o,bF as e}from"./index.5f000719.js";import{a as n}from"./index.e97fdbf8.js";function d(r,a){o(2,arguments);var t=e(a);return n(r,t*12)}export{d as a};
---
+++ assets/useAllCompanyAccess.6808e932.js
@@ -0,0 +1,1 @@
+import{ag as s,aP as e,aQ as c}from"./index.6898036f.js";const n={list(){return s(["company-access"],()=>e.get(`${c}/company-access`).then(a=>a.data))}};export{n as u};
