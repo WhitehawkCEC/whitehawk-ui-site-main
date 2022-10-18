17aa19df9ffa54daac0c4133bacefa7889bbb78a
whdevops codebuild <whdevops@whitehawk.com>
2022-10-18 16:43:52 +0000

    Updates

--- assets/axios-data.ed866f66.js
+++
@@ -1,1 +0,0 @@
-import{am as o}from"./index.fc8bdabe.js";function e(t){return async function(){const a=await o.get(t),{data:n}=a;return n}}export{e as a};
---
+++ assets/index.35219919.js
@@ -0,0 +1,1 @@
+import{an as o,bF as e}from"./index.a39a6b51.js";import{a as n}from"./index.35da7111.js";function d(r,a){o(2,arguments);var t=e(a);return n(r,t*12)}export{d as a};
