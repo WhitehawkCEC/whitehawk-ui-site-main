ddc269acafb54704151ffa5d2295451774f678cf
whdevops codebuild <whdevops@whitehawk.com>
2022-10-20 19:47:58 +0000

    Updates

---
+++ assets/index.10115508.js
@@ -0,0 +1,1 @@
+import{t as r}from"./index.0772cbfc.js";import{an as m}from"./index.bcb96667.js";function n(t,a){m(2,arguments);var i=r(t),f=r(a),e=i.getTime()-f.getTime();return e>0?-1:e<0?1:e}export{n as c};
---
+++ assets/useAllCompanyAccess.e14efc05.js
@@ -0,0 +1,1 @@
+import{ag as s,aP as e,aQ as c}from"./index.bcb96667.js";const n={list(){return s(["company-access"],()=>e.get(`${c}/company-access`).then(a=>a.data))}};export{n as u};
