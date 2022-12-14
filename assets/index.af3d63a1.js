16460b7c8c6cbf857167047adf0fde50cebb8cfb
whdevops codebuild <whdevops@whitehawk.com>
2022-12-14 15:11:05 +0000

    Updates

---
+++ assets/index.af3d63a1.js
@@ -0,0 +1,1 @@
+import{am as i,bG as u}from"./index.86dbf989.js";import{t as g}from"./index.3850133d.js";function m(n,o){i(2,arguments);var t=g(n),a=u(o);if(isNaN(a))return new Date(NaN);if(!a)return t;var r=t.getDate(),e=new Date(t.getTime());e.setMonth(t.getMonth()+a+1,0);var s=e.getDate();return r>=s?e:(t.setFullYear(e.getFullYear(),e.getMonth(),r),t)}export{m as a};
