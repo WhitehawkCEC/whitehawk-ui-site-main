6d89929e4ba941628948386dc9c60d0ab3e3afc7
whdevops codebuild <whdevops@whitehawk.com>
2022-12-16 18:24:11 +0000

    Updates

--- assets/index.e5884948.js
+++
@@ -1,1 +0,0 @@
-import{t as r}from"./index.35bac8e7.js";import{am as n}from"./index.6fa0fa92.js";function g(m,e){n(2,arguments);var t=r(m).getTime(),i=r(e.start).getTime(),a=r(e.end).getTime();if(!(i<=a))throw new RangeError("Invalid interval");return t>=i&&t<=a}export{g as i};
---
+++ assets/usePeratonRiskAnalytics.565f4386.js
@@ -0,0 +1,1 @@
+import{c as a}from"./index.072b1b65.js";import{R as i}from"./index.c8c788fe.js";const u={read(r,o){if(!r.value)throw new Error("Missing ID.");return a(()=>{const e=i.value.find(t=>t.company.id.value===o.value);if(e)return e})},list(r){if(!r.value)throw new Error("Missing ID.");return a(()=>i.value)}};export{u};
