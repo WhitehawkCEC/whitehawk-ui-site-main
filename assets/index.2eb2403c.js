a03c467f996389cda7b71756497b215f535545d9
whdevops codebuild <whdevops@whitehawk.com>
2022-12-20 19:43:20 +0000

    Updates

--- assets/index.2eb2403c.js
+++
@@ -1,1 +0,0 @@
-import{D as r}from"./DynamicForm.7a66b03f.js";import{aY as n}from"./index.4c6249b7.js";const o={value:{type:Object}};function m(t){return n({props:o,render(e){return e(r,{props:{value:this.value,schema:t},attrs:this.$attrs,on:this.$listeners})}})}export{m as d};
