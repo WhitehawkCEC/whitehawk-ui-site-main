5b9285fe377336ab1bc82c952df040dd68784089
whdevops codebuild <whdevops@whitehawk.com>
2022-12-21 16:20:25 +0000

    Updates

---
+++ assets/index.39a5c018.js
@@ -0,0 +1,1 @@
+import{a_ as s,c as l}from"./index.9adc7bbe.js";function u(a){const e=s();return l({get(){return e.value||a.value},set(t){e.value=t}})}export{u as s};
---
+++ assets/useNewestQuestionnaireResponses.31ff88ee.js
@@ -0,0 +1,1 @@
+import{ag as a,aP as n,aQ as t}from"./index.9adc7bbe.js";const r={get(e){return a(["companies",e,"questionnaire-responses","newest"],()=>n.get(`${t}/companies/${e.value}/questionnaire-responses/newest`).then(s=>s.data))}};export{r as u};
