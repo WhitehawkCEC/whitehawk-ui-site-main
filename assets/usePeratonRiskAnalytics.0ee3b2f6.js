63ea6f3d0d2fdb651af25ae406bd5373476d19f6
whdevops codebuild <whdevops@whitehawk.com>
2022-12-22 22:37:35 +0000

    Updates

--- assets/usePeratonRiskAnalytics.0ee3b2f6.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./index.26316912.js";import{R as i}from"./index.a7e3d79d.js";const u={read(r,o){if(!r.value)throw new Error("Missing ID.");return a(()=>{const e=i.value.find(t=>t.company.id.value===o.value);if(e)return e})},list(r){if(!r.value)throw new Error("Missing ID.");return a(()=>i.value)}};export{u};
