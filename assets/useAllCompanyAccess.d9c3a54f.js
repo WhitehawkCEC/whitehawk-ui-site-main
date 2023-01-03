4ec777d014061378e912f72061a1c3c42ca260bb
whdevops codebuild <whdevops@whitehawk.com>
2023-01-03 22:11:58 +0000

    Updates

--- assets/useAllCompanyAccess.d9c3a54f.js
+++
@@ -1,1 +0,0 @@
-import{ah as s,aP as e,aQ as c}from"./index.b52a2fa9.js";const n={list(){return s(["company-access"],()=>e.get(`${c}/company-access`).then(a=>a.data))}};export{n as u};
--- assets/usePeratonRiskAnalytics.0c880d7d.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./index.b52a2fa9.js";import{R as i}from"./index.c356e66d.js";const u={read(r,o){if(!r.value)throw new Error("Missing ID.");return a(()=>{const e=i.value.find(t=>t.company.id.value===o.value);if(e)return e})},list(r){if(!r.value)throw new Error("Missing ID.");return a(()=>i.value)}};export{u};
