7dee52d5fc995d850d3f4a022fd740fee6d8b476
whdevops codebuild <whdevops@whitehawk.com>
2023-01-05 15:54:13 +0000

    Updates

--- assets/index.673d9db2.js
+++
@@ -1,1 +0,0 @@
-import{r as i,bF as u}from"./index.454062da.js";import{t as g}from"./index.c9f29ff6.js";function h(n,o){i(2,arguments);var t=g(n),r=u(o);if(isNaN(r))return new Date(NaN);if(!r)return t;var a=t.getDate(),e=new Date(t.getTime());e.setMonth(t.getMonth()+r+1,0);var s=e.getDate();return a>=s?e:(t.setFullYear(e.getFullYear(),e.getMonth(),a),t)}export{h as a};
