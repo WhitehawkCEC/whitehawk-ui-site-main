5ad510593f166472021c651786d681623e147779
whdevops codebuild <whdevops@whitehawk.com>
2023-01-06 19:14:50 +0000

    Updates

--- assets/index.8549152a.js
+++
@@ -1,1 +0,0 @@
-import{r as n,bF as o}from"./index.bf67f21b.js";import{t as s}from"./index.1891d379.js";function m(a,e){n(2,arguments);var t=s(a),r=o(e);return isNaN(r)?new Date(NaN):(r&&t.setDate(t.getDate()+r),t)}export{m as a};
---
+++ assets/useNewestQuestionnaireResponses.a8e6c769.js
@@ -0,0 +1,1 @@
+import{ah as a,aP as n,aQ as t}from"./index.84346fce.js";const r={get(e){return a(["companies",e,"questionnaire-responses","newest"],()=>n.get(`${t}/companies/${e.value}/questionnaire-responses/newest`).then(s=>s.data))}};export{r as u};
