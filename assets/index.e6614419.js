6e81f96bd60bcb116d9522986608cc66d621b3f2
whdevops codebuild <whdevops@whitehawk.com>
2023-01-06 16:16:05 +0000

    Updates

---
+++ assets/index.e6614419.js
@@ -0,0 +1,1 @@
+import{r as o,bF as e}from"./index.6e202049.js";import{a as s}from"./index.0399a582.js";function d(r,a){o(2,arguments);var t=e(a);return s(r,t*12)}export{d as a};
--- assets/usePeratonRiskAnalytics.39290644.js
+++
@@ -1,1 +0,0 @@
-import{d as a}from"./index.423f83ae.js";import{R as i}from"./index.d7a71c09.js";const u={read(r,o){if(!r.value)throw new Error("Missing ID.");return a(()=>{const e=i.value.find(t=>t.company.id.value===o.value);if(e)return e})},list(r){if(!r.value)throw new Error("Missing ID.");return a(()=>i.value)}};export{u};
