d9a7ccbf4db1c3dbc8a24a4537be955fe2c19b7e
whdevops codebuild <whdevops@whitehawk.com>
2023-01-09 20:10:17 +0000

    Updates

---
+++ assets/useNewestQuestionnaireResponses.a01ba187.js
@@ -0,0 +1,1 @@
+import{ah as a,aP as n,aQ as t}from"./index.b3b2bcca.js";const r={get(e){return a(["companies",e,"questionnaire-responses","newest"],()=>n.get(`${t}/companies/${e.value}/questionnaire-responses/newest`).then(s=>s.data))}};export{r as u};
