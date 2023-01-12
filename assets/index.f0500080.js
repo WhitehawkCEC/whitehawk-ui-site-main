59ecbeb0084d3b8459adff9d71e8c8d889c96ad2
whdevops codebuild <whdevops@whitehawk.com>
2023-01-12 19:08:18 +0000

    Updates

---
+++ assets/index.f0500080.js
@@ -0,0 +1,1 @@
+import{r as o,bG as e}from"./index.b7714616.js";import{a as s}from"./index.8a47fda8.js";function d(r,a){o(2,arguments);var t=e(a);return s(r,t*12)}export{d as a};
--- assets/useBlackKiteEntities.5b502117.js
+++
@@ -1,1 +0,0 @@
-import{ah as a,aP as i,aQ as s}from"./index.37a35517.js";const c={list(e){return a(["companies",e,"black-kite","entities"],()=>i.get(`${s}/companies/${e.value}/black-kite/entities`).then(t=>t.data))}};export{c as u};
