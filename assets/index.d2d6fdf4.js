f832a1953c5341b73c8ef75b3306ac3317012a2c
whdevops codebuild <whdevops@whitehawk.com>
2023-01-12 23:54:57 +0000

    Updates

---
+++ assets/index.d2d6fdf4.js
@@ -0,0 +1,1 @@
+import{r as n,bG as o}from"./index.b1253201.js";import{t as s}from"./index.654ace28.js";function m(a,e){n(2,arguments);var t=s(a),r=o(e);return isNaN(r)?new Date(NaN):(r&&t.setDate(t.getDate()+r),t)}export{m as a};
---
+++ assets/useBitsightCompanies.249de200.js
@@ -0,0 +1,1 @@
+import{ah as s,aP as t,aQ as i}from"./index.b1253201.js";const p={read(a,e){return s(["companies",a,"bitsight","companies",e],()=>t.get(`${i}/companies/${a.value}/bitsight/companies/${e.value}`).then(n=>n.data))},list(a){return s(["companies",a,"bitsight","companies"],()=>t.get(`${i}/companies/${a.value}/bitsight/companies`).then(e=>e.data))}};export{p as u};
